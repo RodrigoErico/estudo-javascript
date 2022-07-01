3905cc144ba29a8dbb4be40734f4b3d933faf663
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-07-01 10:02:58 -0300

    Removendo mais de 1 elemento

--- 3-javascript-arrays/metodo-splice.js
+++ 3-javascript-arrays/metodo-splice.js
@@ -1,4 +1,8 @@
 const names = ['Rodrigo', 'Maria', 'Pedro', 'Ana']
 
-const removeName = names.splice(3, 1)
+// const removeName = names.splice(3, 1)
+// or
+// const removeName = names.splice(-1, 1) // índice e elemento
+
+const removeName = names.splice(-2, 2)
 console.log(names, removeName)
