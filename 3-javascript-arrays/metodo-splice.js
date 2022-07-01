20d058c474d900645468cf49db277ebb1d9f2edb
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-07-01 10:25:28 -0300

    Criou um array vazio e adicionou mais um elemento no array

--- 3-javascript-arrays/metodo-splice.js
+++ 3-javascript-arrays/metodo-splice.js
@@ -1,8 +1,12 @@
 const names = ['Rodrigo', 'Maria', 'Pedro', 'Ana']
-
 // const removeName = names.splice(3, 1)
 // or
 // const removeName = names.splice(-1, 1) // índice e elemento
-
 const removeName = names.splice(-2, 2)
 console.log(names, removeName)
+
+
+const names1 = ['Rodrigo', 'Maria', 'Pedro', 'Ana']
+
+const addName = names1.splice(3, 0, 'Luke')
+console.log(names1, addName)
