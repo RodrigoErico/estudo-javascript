ac7797c68249e8e0e6dfa9b15ad19a524d49a3a9
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-07-01 10:12:35 -0300

    Removendo commit

--- 3-javascript-arrays/metodo-splice.js
+++ 3-javascript-arrays/metodo-splice.js
@@ -3,6 +3,6 @@ const names = ['Rodrigo', 'Maria', 'Pedro', 'Ana']
 // const removeName = names.splice(3, 1)
 // or
 // const removeName = names.splice(-1, 1) // índice e elemento
-
+// teste commit
 const removeName = names.splice(-2, 2)
 console.log(names, removeName)
