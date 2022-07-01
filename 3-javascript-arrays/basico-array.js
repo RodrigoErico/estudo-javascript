a279167e1ccd0318b3ed4dccbc09b77497e1e3dc
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-07-01 09:41:46 -0300

    Join - Unindo um array

--- 3-javascript-arrays/basico-array.js
+++ 3-javascript-arrays/basico-array.js
@@ -1,3 +1,7 @@
-const name = 'Rodrigo Erico Antonio';
-const names = name.split(' ');
+//const name = 'Rodrigo Erico Antonio';
+//const names = name.split(' ');
+//console.log(names)
+
+const name = ['Rodrigo', 'Erico', 'Antonio'];
+const names = name.join(' ');
 console.log(names)
