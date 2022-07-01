b493e5d290fe24fc0f5a60f6fa1e7ddf8b24987b
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-07-01 10:28:16 -0300

    Removeu dois elementos e adicionou um elemento

--- 3-javascript-arrays/metodo-splice.js
+++ 3-javascript-arrays/metodo-splice.js
@@ -2,15 +2,12 @@ const names = ['Rodrigo', 'Maria', 'Pedro', 'Ana']
 // const removeName = names.splice(3, 1)
 // or
 // const removeName = names.splice(-1, 1) // índice e elemento
-<<<<<<< HEAD
-=======
 // teste commit
->>>>>>> ac7797c68249e8e0e6dfa9b15ad19a524d49a3a9
 const removeName = names.splice(-2, 2)
 console.log(names, removeName)
 
 
 const names1 = ['Rodrigo', 'Maria', 'Pedro', 'Ana']
 
-const addName = names1.splice(3, 0, 'Luke')
+const addName = names1.splice(2, 2, 'Luke')
 console.log(names1, addName)
